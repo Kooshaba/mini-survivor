37031f9b947b4981e988801ed345b219f908cc5d
Andy Cernera <kooshaba@gmail.com>
2024-04-09 17:25:34 +0900

    slower fast boy

--- src/game/scenes/GameScene/sprites/FastBoy.ts
+++ src/game/scenes/GameScene/sprites/FastBoy.ts
@@ -4,8 +4,8 @@ import { Enemy } from "./Enemy";
 export class FastBoy extends Enemy {
   constructor(scene: Game, x: number, y: number) {
     super(scene, x, y);
-    this.baseSpeed = 160;
-    this.speed = 160;
+    this.baseSpeed = 130;
+    this.speed = 130;
     this.baseTint = 0x00ff00;
     this.health = 15;
     this.totalHealth = 15;
